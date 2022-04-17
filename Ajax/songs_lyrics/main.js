84f1d601120e4301b0d98342d6cb8b5c7844781b
martinspasof <martin.spasov@opencode.com>
2022-04-17 20:53:16 +0300

    Loop images(btns) and fetch requests

--- Ajax/songs_lyrics/main.js
+++ Ajax/songs_lyrics/main.js
@@ -1,9 +1,12 @@
 var nodes = {
+	head: document.querySelector('.head'),
 	output: document.getElementsByClassName('output')[0],
 	getDataBtn1: document.getElementsByClassName('getDataBtn')[0],
 	getDataBtn2: document.getElementsByClassName('getDataBtn')[1],
 	getDataBtn3: document.getElementsByClassName('getDataBtn')[2],
+	getDataBtns: document.getElementsByClassName('getDataBtn'),
 };
+
 
 var AJAXcall = function (callback, getDataBtn, ...args) {
 	// create a XMLHttpRequest object
@@ -32,14 +35,54 @@ function render(data, node) {
 	node.innerHTML = data
 }
 
-nodes.getDataBtn1.addEventListener('click', function () {
-	AJAXcall(render, 'getDataBtn1', nodes);
-});
+function processResponse(r) {
+	if(r.status === 200){
+		return r.text()
+	}else{
+		throw new Error(`Error: ${r.status}`)
+	}
+}
 
-nodes.getDataBtn2.addEventListener('click', function () {
-	AJAXcall(render, 'getDataBtn2', nodes);
-});
+// loop of button elements (images)
+// fetch requests
+for (let btns of nodes.getDataBtns) {
+	let btnEl = btns.dataset.imgId;
+	nodes[btnEl].addEventListener('click', function (e) {
+		const imgId = this.dataset.imgId;
 
-nodes.getDataBtn3.addEventListener('click', function () {
-	AJAXcall(render, 'getDataBtn3', nodes);
-});
+		// AJAXcall(render, imgId, nodes);
+
+		var URL = this.src.replace(/(\w+)\.[a-z]+$/, '$1.txt').replace(/\bimages\b/, 'data');
+
+		fetch(URL).then(res => {
+			return processResponse(res);
+		}).then(respText => {
+			render(respText, nodes.output);
+			return respText;
+
+		}).catch(err => {
+			console.log(err);
+		})
+
+	});
+}
+
+// loop of button elements (images)
+// for (let btns of nodes.getDataBtns) {
+// 	let btnEl = btns.dataset.imgId;
+// 	nodes[btnEl].addEventListener('click', function (e) {
+// 		const imgId = this.dataset.imgId;
+
+// 		AJAXcall(render, imgId, nodes);
+
+// 	});
+// }
+
+// nodes.head.addEventListener('click', function(e) {
+// 	const img = e.target;
+// 	console.log(img.src);
+// 	const imgId = img.dataset.imgId;
+
+// 	AJAXcall(render, imgId, nodes);
+
+// })
